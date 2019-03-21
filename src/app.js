a166b8fdd6c94d3f21007e869cbc831aa3c2b3e4
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-21 10:00:29 +0000

    changed port

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ const fact = require("./factaday");
 
 const publicDirectory = path.join(__dirname, "../public");
 app.use(express.static(publicDirectory));
+const port = process.env.PORT || 3000;
 
 app.get("/fact", (req, res) => {
   fact(req.query.month, req.query.day).then(response => res.send(response));
 });
 
-app.listen(80, () => {
+app.listen(port, () => {
   console.log("listening on port 3000");
 });
